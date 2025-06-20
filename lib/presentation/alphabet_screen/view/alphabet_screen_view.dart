c9c6e27f3c933416602c1f3e9501053de6eb8fa5
Hamza Shahid <hamzabutthb553.hb@gmail.com>
2025-06-20 12:42:56 +0500

    initial

--- lib/presentation/alphabet_screen/view/alphabet_screen_view.dart
+++ lib/presentation/alphabet_screen/view/alphabet_screen_view.dart
@@ -4,7 +4,6 @@ import 'package:get/get.dart';
 import '../../../core/widgets/custom_appBar.dart';
 import '../../../core/widgets/text_widget.dart';
 
-
 class AlphabetScreenView extends StatefulWidget {
   const AlphabetScreenView({super.key});
 
@@ -17,7 +16,6 @@ class _AlphabetScreenViewState extends State<AlphabetScreenView> {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: PreferredSize(
@@ -33,9 +31,10 @@ class _AlphabetScreenViewState extends State<AlphabetScreenView> {
         if (controller.alphabetCateg.isEmpty) {
           return Center(child: CircularProgressIndicator());
         } else {
-          final filteredList = controller.alphabetCateg.where((category) {
-            return category['category_name'] == 'vocabulary2';
-          }).toList();
+          final filteredList =
+              controller.alphabetCateg.where((category) {
+                return category['category_name'] == 'vocabulary2';
+              }).toList();
           return ListView.builder(
             shrinkWrap: true,
             physics: NeverScrollableScrollPhysics(),
@@ -47,31 +46,36 @@ class _AlphabetScreenViewState extends State<AlphabetScreenView> {
                 decoration: BoxDecoration(
                   color: Colors.grey.shade100.withValues(alpha: .7),
                   borderRadius: BorderRadius.circular(10),
-                  border: Border.all(color: Colors.grey.shade200)
+                  border: Border.all(color: Colors.grey.shade200),
                 ),
                 child: ListTile(
                   leading: CircleAvatar(
-                      backgroundColor: Colors.white,
-                      radius: 14,
-                      child: regularText(textTitle: '${index+1}', textSize: 16, textColor: Colors.black)),
-                  title: regularText(
-                      textTitle: category['english_words'] ?? '',
-                      textSize: 18,
+                    backgroundColor: Colors.white,
+                    radius: 14,
+                    child: regularText(
+                      textTitle: '${index + 1}',
+                      textSize: 16,
                       textColor: Colors.black,
-                      textWeight: FontWeight.w600
+                    ),
                   ),
-                  subtitle:  regularText(
+                  title: regularText(
+                    textTitle: category['english_words'] ?? '',
+                    textSize: 18,
+                    textColor: Colors.black,
+                    textWeight: FontWeight.w600,
+                  ),
+                  subtitle: regularText(
                     textTitle: category['urdu_words'] ?? '',
                     textSize: 16,
                     textColor: Colors.blue,
                   ),
-                  trailing: Icon(Icons.play_circle, color: Colors.blue,),
+                  trailing: Icon(Icons.play_circle, color: Colors.blue),
                 ),
               );
             },
           );
         }
-      })
+      }),
     );
   }
 }
--- lib/presentation/phrases_screen/view/phrases_view.dart
+++ lib/presentation/phrases_screen/view/phrases_view.dart
@@ -11,8 +11,8 @@ import '../../../core/widgets/custom_toast.dart';
 import '../../../core/widgets/icon_buttons.dart';
 import '../../vocabulary_screen/controller/vocabulary_controller.dart';
 import '../controller/phrases_controller.dart';
-class PhrasesCategoryScreen extends StatefulWidget {
 
+class PhrasesCategoryScreen extends StatefulWidget {
   @override
   State<PhrasesCategoryScreen> createState() => _PhrasesCategoryScreenState();
 }
@@ -23,95 +23,95 @@ class _PhrasesCategoryScreenState extends State<PhrasesCategoryScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        // appBar: PreferredSize(
-        //   preferredSize: Size(0, 60),
-        //   child: CustomAppBar(
-        //     title: 'English Phrases',
-        //
-        //     textColor: Colors.white,
-        //     textSize: 24,
-        //     textWeight: FontWeight.w400,
-        //   ),
-        // ),
-        body: Stack(
-          children: [
-            const BackgroundCircles(),
-            Positioned(
-              top: 40,
-              left: 16,
-              right: 16,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  BackIconButton(),
-                  const SizedBox(width:15),
-                  Expanded(
-                    child: Text(
-                      "Güncel Durum",
-                      style: const TextStyle(
-                        fontSize: 22,
-                        fontWeight: FontWeight.w600,
-                        color: Colors.white,
-                      ),
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
+      // appBar: PreferredSize(
+      //   preferredSize: Size(0, 60),
+      //   child: CustomAppBar(
+      //     title: 'English Phrases',
+      //
+      //     textColor: Colors.white,
+      //     textSize: 24,
+      //     textWeight: FontWeight.w400,
+      //   ),
+      // ),
+      body: Stack(
+        children: [
+          const BackgroundCircles(),
+          Positioned(
+            top: 40,
+            left: 16,
+            right: 16,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                BackIconButton(),
+                const SizedBox(width: 15),
+                Expanded(
+                  child: Text(
+                    "Güncel Durum",
+                    style: const TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.white,
                     ),
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-            Padding(
-              padding:  EdgeInsets.only(top:80, left: kBodyHp, right: kBodyHp),
-              child: Obx(() {
-                if (controller.categories.isEmpty) {
-                  return Center(child: CircularProgressIndicator());
-                } else {
-                  final filteredList = controller.categories.where((category) {
-                    return category['category_name'] == 'Phrases';
-                  }).toList();
-                  return ListView.builder(
-                    shrinkWrap: true,
-                    // physics: NeverScrollableScrollPhysics(),
-                    itemCount: filteredList.length,
-                    itemBuilder: (context, index) {
-                      final category = filteredList[index];
-                      return Container(
-                        decoration: roundedDecoration,
-                        width: double.maxFinite,
-                        // padding: const EdgeInsets.all(10),
-                        margin: EdgeInsets.symmetric(horizontal: 10, vertical: 6),
-                        /*decoration: BoxDecoration(
-                            color: Colors.grey.shade100.withValues(alpha: .7),
-                            borderRadius: BorderRadius.circular(10),
-                            border: Border.all(color: Colors.grey.shade200)
-                        ),*/
-                        child: ListTile(
-                          leading: CircleAvatar(
-                              backgroundColor: Colors.white,
-                              radius: 14,
-                              child: regularText(textTitle: '${index+1}', textSize: 16, textColor: Colors.black)),
-                          trailing: Icon(Icons.play_circle, color: Colors.blue,),
-                          title: regularText(
-                              textTitle: category['english_words'] ?? '',
-                              textSize: 18,
-                              textColor: Colors.black,
-                              textWeight: FontWeight.w600
-                          ),
-                          subtitle:  regularText(
-                            textTitle: category['urdu_words'] ?? '',
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 80, left: kBodyHp, right: kBodyHp),
+            child: Obx(() {
+              if (controller.categories.isEmpty) {
+                return Center(child: CircularProgressIndicator());
+              } else {
+                final filteredList =
+                    controller.categories.where((category) {
+                      return category['category_name'] == 'Phrases';
+                    }).toList();
+                return ListView.builder(
+                  shrinkWrap: true,
+                  // physics: NeverScrollableScrollPhysics(),
+                  itemCount: filteredList.length,
+                  itemBuilder: (context, index) {
+                    final category = filteredList[index];
+                    return Container(
+                      decoration: roundedDecoration,
+                      width: double.maxFinite,
+                      // padding: const EdgeInsets.all(10),
+                      margin: EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+                      child: ListTile(
+                        leading: CircleAvatar(
+                          backgroundColor: Colors.white,
+                          radius: 14,
+                          child: regularText(
+                            textTitle: '${index + 1}',
                             textSize: 16,
-                            textColor: Colors.blue,
+                            textColor: Colors.black,
                           ),
-
                         ),
-                      );
-                    },
-                  );
-                }
-              }),
-            ),
-          ],
-        )
+                        trailing: Icon(Icons.play_circle, color: Colors.blue),
+                        title: regularText(
+                          textTitle: category['english_words'] ?? '',
+                          textSize: 18,
+                          textColor: Colors.black,
+                          textWeight: FontWeight.w600,
+                        ),
+                        subtitle: regularText(
+                          textTitle: category['urdu_words'] ?? '',
+                          textSize: 16,
+                          textColor: Colors.blue,
+                        ),
+                      ),
+                    );
+                  },
+                );
+              }
+            }),
+          ),
+        ],
+      ),
     );
   }
 }
